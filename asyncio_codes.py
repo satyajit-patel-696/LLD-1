05df6a4f0618da09fe5410e3e0a2afb0dc188cec
satyajit <satyajeetpatel96@gmail.com>
2026-04-16 15:59:44 +0530

    add

--- asyncio_codes.py
+++ asyncio_codes.py
@@ -4,4 +4,22 @@ async def greet(name):
     print(f"Hello, {name}!")
     await asyncio.sleep(1)
     print(f"Goodbye, {name}!")
-asyncio.run(greet("Alice"))
+#asyncio.run(greet("Alice"))
+
+async def main():
+    await asyncio.gather(
+        greet("Alice"),
+        greet("Bob"),
+        greet("Charlie")
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+#outputs:
+#Hello, Alice!
+#Hello, Bob!        
+#Hello, Charlie!
+#Goodbye, Alice!        
+#Goodbye, Bob!
+#Goodbye, Charlie!
