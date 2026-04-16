f5b321edd328918bd66e1a94cc458e0026f03f7f
satyajit <satyajeetpatel96@gmail.com>
2026-04-16 18:35:18 +0530

    adding decorator simple

---
+++ decorator.py
@@ -0,0 +1,24 @@
+import time
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter() # get the start time
+        print(f"start time: {start_time}")
+        
+        result = func(*args, **kwargs)  # get "hello"
+        
+        print(result)   # print it in middle
+        
+        end_time = time.perf_counter()  # get the end time
+        print(f"end time: {end_time}")
+        print(f"execution time: {end_time - start_time}")
+
+        return result   # still return if needed
+    return wrapper
+
+
+@my_decorator
+def say_hello():
+    return "hello"
+
+
+say_hello()
